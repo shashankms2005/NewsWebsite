e3f820fc34723d7f99840f329dcc941e839319b7
Shashank MS <shashupreethims@gmail.com>
2023-12-25 13:35:15 +0530

    1st commit

---
+++ index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+const port = 3000;
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "hello" });
+});
+
+app.post("/topic-search", async (req, res) => {
+  const q = req.body.topic;
+  const s = req.body.Sort;
+  const n = req.body.num;
+  try {
+    const topic = await axios.get(
+      `https://newsapi.org/v2/everything?q=${q}&language=en&pageSize=${n}&sortBy=${s}&apiKey=c83d15300a934ef086c96864fae2667a`
+    );
+    res.render("index.ejs", { content1: JSON.stringify(topic.data) });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.post("/topic-headlines", async (req, res) => {
+  const country = req.body.country;
+  const category = req.body.category;
+  const num = req.body.hnum;
+  try {
+    const topic = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${num}&apiKey=c83d15300a934ef086c96864fae2667a`
+    );
+    res.render("index.ejs", { content2: JSON.stringify(topic.data) });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.post("/topic-sources", async (req, res) => {
+  const country2 = req.body.country2;
+  try {
+    const topic = await axios.get(
+      `https://newsapi.org/v2/top-headlines/sources?country=${country2}&category=general&apiKey=c83d15300a934ef086c96864fae2667a`
+    );
+    res.render("index.ejs", { content3: JSON.stringify(topic.data) });
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening to port ${3000} .`);
+});
